54c2dc114e35c4a1c75abeb7b83de29a8d662b10
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-07-04 17:35:35 -0700

    add conditional for registered user

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,20 @@ from .forms import SignUpForm
 
 # Create your views here.
 def signup(request):
-    signup_form = SignUpForm()
+    registered = False
+    if request.method == 'POST':
+        signup_form = SignUpForm(data=request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+    else:
+        signup_form = SignUpForm()
     context = {
     'title': 'Sign up!',
-    'signup_form': signup_form
+    'signup_form': signup_form,
+    'registered': registered,
     }
     return render(request, 'signup.html', context)
 
