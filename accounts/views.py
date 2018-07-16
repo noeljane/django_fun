515dd06aff06d579d49d29cdb19e63a01a4e35f5
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-07-16 23:22:25 +1000

    fix logout

--- accounts/views.py
+++ accounts/views.py
@@ -43,6 +43,3 @@ def logout(request):
     if request.method == 'POST':
         auth.logout(request)
         return redirect('home')
-
-def profile(request):
-    return render(request, 'profile.html', {'title': 'Profile, dude!'})
