23dc3a24ab09390cc98b9ee611b351e2e93a367b
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-07-21 13:00:22 +1000

    add startup model to startups.models

--- startups/models.py
+++ startups/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Startup(models.Model):
+    name = models.CharField(max_length=200)
+    tagline = models.TextField()
+    photo = models.TextField()
+    founder = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=1)
