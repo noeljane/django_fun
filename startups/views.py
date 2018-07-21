c5b6fc6f3564f4aed8f578f3f16f7087abe09632
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-07-21 13:16:19 +1000

    write create view function

--- startups/views.py
+++ startups/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import StartupForm
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def create(request):
+    startup_form = StartupForm()
+
+    return render(request, 'create.html', {
+    'title': 'Make a startup',
+    'startup_form':startup_form
+    })
