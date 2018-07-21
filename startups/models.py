35413df8b44d240e79bd4829763972f53dc9b260
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-07-21 13:11:32 +1000

    add form to startups.forms

--- startups/models.py
+++ startups/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class Startup(models.Model):
     name = models.CharField(max_length=200)
     tagline = models.TextField()
+    description = models.TextField()
     photo = models.TextField()
     founder = models.ForeignKey(User, on_delete=models.CASCADE)
+
     likes = models.IntegerField(default=1)
+    founded_date = models.DateTimeField(default=timezone.now)
