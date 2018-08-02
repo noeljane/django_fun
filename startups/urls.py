de8c25f53343345bfc7e693700d46dba4d2b27b4
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-02 15:04:11 -0700

    fix reverse match bug with delete

--- startups/urls.py
+++ startups/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 path('',views.home, name='home'),
-path('create', views.create, name='create')
+path('create', views.create, name='create'),
+path('<int:startup_id>/delete/', views.delete, name='delete'),
 ]
