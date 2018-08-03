8247faf6f141c9a8f3d6b86c1333b7dd70a288aa
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-03 16:28:28 -0700

    connect static files

--- django_fun/urls.py
+++ django_fun/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('startups.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]
+] 
