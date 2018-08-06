864f09fb5d5da882016b9ca82d2d4e0bcd629914
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-06 16:03:37 -0700

    name form submit works

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,11 @@ $(function () {
   console.log("It's working!!!!!")
 
   //Variables
-  let $oldName = $('#oldName')
-  console.log($oldName.val());
+  let $oldName = $('#oldName');
+  let $nameForm = $('#nameForm')
+
+
+  $nameForm.on('submit', getNewName)
 
   //This function takes the vowels out of a string
   function shortcut(string) {
@@ -19,9 +22,22 @@ $(function () {
   };
 
   // Get info from Name Field
+    //Write function that obtains the value of the input in the form
+    //set variable of name to pass to the shortcut function
+  function getNewName(evt) {
+    evt.preventDefault()
+    console.log("I'm running a function now.");
+    
+
+  };
+
 
 
   // Run function to get new Name
   //Set value for form tag as the new name
-  //Generate Tagline for new name using
+  //Generate Tagline for new name using function from before
+
+  //API call to the Unsplash API to get an image that matches one of the nouns
+
+
 });
