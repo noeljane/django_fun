e5547d50feeee4b59ba24e0e56280e56cdd68f70
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-06 15:47:48 -0700

    jQuery is working

--- static/js/main.js
+++ static/js/main.js
@@ -3,23 +3,25 @@ $(function () {
   console.log("It's working!!!!!")
 
   //Variables
-  
+  let $oldName = $('#oldName')
+  console.log($oldName.val());
 
   //This function takes the vowels out of a string
   function shortcut(string) {
-    let newString = "";
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let newString = ""
+    let vowels = ['a', 'e', 'i', 'o', 'u']
     for (let i = 0; i < string.length; i++) {
       if (!vowels.includes(string[i])){
-        newString += string[i];
-    };
+        newString += string[i]
+      }
+    }
     return newString
-
   };
 
   // Get info from Name Field
 
+
   // Run function to get new Name
   //Set value for form tag as the new name
   //Generate Tagline for new name using
-};
+});
