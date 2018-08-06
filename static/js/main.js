87b1a226bd5482571eeb84f0a109bb455a68be67
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-06 15:36:37 -0700

    add remove vowels code

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,25 @@
-console.log("It's working!!!!!")
+
+$(function () {
+  console.log("It's working!!!!!")
+
+  //Variables
+  
+
+  //This function takes the vowels out of a string
+  function shortcut(string) {
+    let newString = "";
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let i = 0; i < string.length; i++) {
+      if (!vowels.includes(string[i])){
+        newString += string[i];
+    };
+    return newString
+
+  };
+
+  // Get info from Name Field
+
+  // Run function to get new Name
+  //Set value for form tag as the new name
+  //Generate Tagline for new name using
+};
