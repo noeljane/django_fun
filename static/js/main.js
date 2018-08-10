8a7e58d780553bbef049fab7c642a74a93b725af
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-10 10:00:05 -0700

    refactor tagline function

--- static/js/main.js
+++ static/js/main.js
@@ -7,9 +7,9 @@ $(function () {
   let $nameForm = $('#nameForm');
   let $newName = $('#name');
   let $taglineForm = $('#taglineForm');
+  let $newTagline = $('#tagline')
 
   let $noun = $('#noun')
-  let $noun = noun.val()
 
 
 
@@ -25,40 +25,42 @@ $(function () {
     return newString
   };
 
-  // Get info from Name Field & Place in the Startup Form
-  function getRouletteName(evt) {
-    evt.preventDefault();
-    let rouletteName = shortcut($oldName.val())
-    console.log(rouletteName)
-    //Set value for form tag as the new name
-    $newName.val(rouletteName)
+  //Generate Tagline for new name using function I wrote before:
+  var startups = ["Uber", "AirBnb", "Tinder", "Grindr", "Shazam", "Pandora", "Spotify", "Instagram", "Facebook", "MySpace", "Rover", "Blue Apron", "SnapChat", "Google" ]
+
+  //Make a Random Number Function
+
+  function randomNumber(num){
+    return Math.floor((Math.random() * num))
   };
+
+
+  function makeTagline (){
+
+    let i = randomNumber(startups.length)
+    let startupName = startups[i]
+
+    return `${startupName} for ${$noun.val()}`
+
+  };
+
+  // Get info from Name Field & Place in the Startup Form
+  function getRouletteName(evt, userNoun) {
+    evt.preventDefault();
+    let rouletteName = shortcut($oldName.val());
+    console.log(rouletteName);
+    makeTagline($noun.val());
+    console.log(makeTagline($noun.val()));
+    //Set value for form tag as the new name
+    $newName.val(rouletteName);
+  };
+
+
 
 
   // Run function to get new Name
   $nameForm.on('submit', getRouletteName)
 
-  //Generate Tagline for new name using function I wrote before:
-  var startups = ["Uber", "AirBnb", "Tinder", "Grindr", "Shazam", "Pandora", "Spotify", "Instagram", "Facebook", "MySpace", "Rover", "Blue Apron", "SnapChat", "Google" ]
-
-//Make a Random Number Function
-
-function randomNumber(num){
-  return Math.floor((Math.random() * num))
-};
-
-
-function makeTagline (noun){
-  let noun = $noun
-
-  let i = randomNumber(startups.length)
-  let startupName = startups[i]
-
-  return `${startupName} for ${noun}`
-
-};
-
-console.log(makeTagline())
 
 
 
