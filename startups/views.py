d0965a29ba8a1c34200055568c634675eefa2848
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-13 15:03:25 -0700

    move api request to backend

--- startups/views.py
+++ startups/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import StartupForm
 from .models import Startup
+import requests
+from django.django_fun.settings import CLIENT_ID
 
 # Create your views here.
 def home(request):
@@ -10,9 +12,10 @@ def home(request):
     'startups': Startup.objects.order_by('-likes'),
     }
     return render(request, 'home.html', context)
-
 @login_required()
 def create(request):
+    response = requests.get('https://api.unsplash.com/photos/search/?client_id=${CLIENT_ID}&query=puppies&page=1')
+    unsplash_data = response.json()
     startup_form = StartupForm()
     if request.method == 'POST':
         startup_form = StartupForm(data=request.POST)
@@ -27,7 +30,8 @@ def create(request):
 
     return render(request, 'create.html', {
     'title': 'Make a startup',
-    'startup_form':startup_form
+    'startup_form':startup_form,
+    'id':unsplash_data[0][id]
     })
 
 @login_required
